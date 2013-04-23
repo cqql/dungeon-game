d13626b083e66bca6ae5493f12a51202c239fdc0
Louise Baumeister <louise_baumeister@gmx.de>
2013-04-23 16:02:07 +0200

    Test

--- src/main/java/dungeon/Main.java
+++ src/main/java/dungeon/Main.java
@@ -2,6 +2,6 @@ package dungeon;
 
 public class Main {
   public static void main (String[] args) {
-    System.out.println("Welcome to the dungeon!");
+    System.out.println("Welcome to the dungeon - von Louise");
   }
 }
