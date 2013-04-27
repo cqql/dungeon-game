0d843b34357135512f5ae27f24d8638263a2fb74
Jin Kudo <nhj@gmx.net>
2013-04-27 23:09:13 +0200

    Add forecast

--- src/main/java/dungeon/Main.java
+++ src/main/java/dungeon/Main.java
@@ -3,5 +3,6 @@ package dungeon;
 public class Main {
   public static void main (String[] args) {
     System.out.println("Welcome to the dungeon - von Louise");
+    System.out.println("It's gonna be awesome!");
   }
 }
