8021b8bd48093961ac853a05eefd4c0972c71ba1
Jin Kudo <nhj@gmx.net>
2013-05-08 14:40:55 +0200

    implement models

--- src/main/java/dungeon/models/Room.java
+++ src/main/java/dungeon/models/Room.java
@@ -1,5 +1,10 @@
 package dungeon.models;
 
+import java.util.List;
+
 public class Room {
 
+  public List<Enemy> enemies;
+
+  public List<List<Tile>> tiles;
 }
--- src/main/java/dungeon/models/TeleporterTile.java
+++ src/main/java/dungeon/models/TeleporterTile.java
@@ -2,4 +2,14 @@ package dungeon.models;
 
 public class TeleporterTile extends Tile {
 
+  public boolean blocking = false;
+
+  public static class Target {
+    String RoomID;
+    public int x;
+    public int y;
+  }
+
+  public Target target;
+
 }
