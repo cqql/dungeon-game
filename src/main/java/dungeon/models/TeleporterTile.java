6aa11b33e6c537a3cded33122a112c3f2dfa40a8
Jin Kudo <nhj@gmx.net>
2013-05-08 16:27:47 +0200

    corrected spelling of "roomID"

--- src/main/java/dungeon/models/TeleporterTile.java
+++ src/main/java/dungeon/models/TeleporterTile.java
@@ -5,7 +5,7 @@ public class TeleporterTile extends Tile {
   public boolean blocking = false;
 
   public static class Target {
-    String RoomID;
+    String roomID;
     public int x;
     public int y;
   }
