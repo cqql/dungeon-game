5606dff5206c50f7f7b6bfde52cfab054451272c
Marten Lienen <marten.lienen@gmail.com>
2013-05-13 09:13:44 +0200

    Give the world a player

--- src/main/java/dungeon/models/World.java
+++ src/main/java/dungeon/models/World.java
@@ -7,7 +7,10 @@ import java.util.List;
 public class World {
   public final List<Room> rooms;
 
-  public World (List<Room> rooms) {
+  public final Player player;
+
+  public World (List<Room> rooms, Player player) {
     this.rooms = Collections.unmodifiableList(new ArrayList<Room>(rooms));
+    this.player = player;
   }
 }
