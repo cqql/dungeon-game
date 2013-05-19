0239469ae322d98570e48605eba205018f883e82
Marten Lienen <marten.lienen@gmail.com>
2013-05-19 16:56:35 +0200

    Add events for winning and losing

---
+++ src/main/java/dungeon/game/messages/DefeatEvent.java
@@ -0,0 +1,10 @@
+package dungeon.game.messages;
+
+import dungeon.messages.Message;
+
+/**
+ * The game logic sends this when you have been defeated.
+ */
+public class DefeatEvent implements Message {
+
+}
