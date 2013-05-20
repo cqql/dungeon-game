ee5595c8b9be0fdc001faf85edfd3012bc8d7b77
Marten Lienen <marten.lienen@gmail.com>
2013-05-20 11:17:54 +0200

    Allow multiple keys to be pressed at the same time

--- src/main/java/dungeon/ui/InputToMessageConverter.java
+++ src/main/java/dungeon/ui/InputToMessageConverter.java
@@ -1,16 +1,27 @@
 package dungeon.ui;
 
-import dungeon.ui.messages.MoveCommand;
 import dungeon.messages.Mailman;
+import dungeon.ui.messages.MoveCommand;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.HashSet;
+import java.util.Set;
 
+/**
+ * This converts input events like key presses in internal messages that can then be interpreted by the other modules.
+ *
+ * To support multiple keys pressed at the same time, it manages a set of currently pressed keys.
+ */
 public class InputToMessageConverter implements KeyListener {
   private final Mailman mailman;
 
+  private final Set<Character> activeKeys;
+
   public InputToMessageConverter (Mailman mailman) {
     this.mailman = mailman;
+
+    this.activeKeys = new HashSet<>();
   }
 
   @Override
@@ -20,7 +31,23 @@ public class InputToMessageConverter implements KeyListener {
 
   @Override
   public void keyPressed (KeyEvent keyEvent) {
-    switch (keyEvent.getKeyChar()) {
+    this.activeKeys.add(keyEvent.getKeyChar());
+
+    for (Character key : this.activeKeys) {
+      this.sendCommand(key);
+    }
+  }
+
+  @Override
+  public void keyReleased (KeyEvent keyEvent) {
+    this.activeKeys.remove(keyEvent.getKeyChar());
+  }
+
+  /**
+   * Send the command that is associated with the #key.
+   */
+  private void sendCommand (Character key) {
+    switch (key) {
       case 'w':
         this.mailman.send(MoveCommand.UP);
         break;
@@ -35,10 +62,5 @@ public class InputToMessageConverter implements KeyListener {
         break;
       default:
     }
-  }
-
-  @Override
-  public void keyReleased (KeyEvent keyEvent) {
-
   }
 }
