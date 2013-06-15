f86c285dffe3db60ade3c94f0d03c3ba5af3d4d4
Marten Lienen <marten.lienen@gmail.com>
2013-06-15 14:12:59 +0200

    Add Item#id attribute

--- src/main/java/dungeon/load/adapters/ItemAdapter.java
+++ src/main/java/dungeon/load/adapters/ItemAdapter.java
@@ -2,17 +2,22 @@ package dungeon.load.adapters;
 
 import dungeon.models.Item;
 
+import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.adapters.XmlAdapter;
 
 public class ItemAdapter extends XmlAdapter<ItemAdapter, Item> {
+  @XmlAttribute
+  public int id;
+
   @Override
   public Item unmarshal (ItemAdapter itemAdapter) throws Exception {
-    return new Item();
+    return new Item(itemAdapter.id);
   }
 
   @Override
   public ItemAdapter marshal (Item item) throws Exception {
     ItemAdapter adapter = new ItemAdapter();
+    adapter.id = item.getId();
 
     return adapter;
   }
--- src/main/java/dungeon/models/Item.java
+++ src/main/java/dungeon/models/Item.java
@@ -4,5 +4,13 @@ package dungeon.models;
  * This can be any item, that could exist - A sword, a shield, a potion, etc.
  */
 public class Item {
+  private final int id;
 
+  public Item (int id) {
+    this.id = id;
+  }
+
+  public int getId () {
+    return this.id;
+  }
 }
