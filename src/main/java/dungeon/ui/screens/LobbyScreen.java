f7bdb1ebf77e528211f5500cb3fae7c2794c812f
Marten Lienen <marten.lienen@gmail.com>
2013-07-13 15:22:55 +0200

    Enable chatting in lobby

--- src/main/java/dungeon/ui/screens/LobbyScreen.java
+++ src/main/java/dungeon/ui/screens/LobbyScreen.java
@@ -3,21 +3,87 @@ package dungeon.ui.screens;
 import dungeon.client.Client;
 import dungeon.messages.Message;
 import dungeon.messages.MessageHandler;
+import dungeon.models.World;
+import dungeon.ui.messages.ChatMessage;
+import dungeon.ui.messages.ShowLobby;
 
 import javax.swing.*;
+import javax.swing.event.MouseInputAdapter;
+import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LobbyScreen extends JPanel implements MessageHandler {
+  private final Font chatFont = new Font("Arial", Font.PLAIN, 20);
+
+  private final JButton backButton = new JButton("Zurück");
+
+  private final JButton readyButton = new JButton("Bereit");
+
+  private final JTextArea playerList = new JTextArea();
+
+  private final JTextArea chatMessageList = new JTextArea();
+
+  private final JButton messageButton = new JButton("Nachricht schreiben");
+
+  private final List<ChatMessage> chatMessages = new ArrayList<>();
+
   private final Client client;
 
   public LobbyScreen (Client client) {
     super(new GridLayout(5, 1));
 
     this.client = client;
+
+    this.add(this.backButton);
+    this.add(this.readyButton);
+    this.add(this.playerList);
+    this.add(this.chatMessageList);
+    this.add(this.messageButton);
+
+    this.playerList.setEnabled(false);
+
+    this.chatMessageList.setEnabled(false);
+    this.chatMessageList.setFont(this.chatFont);
+
+    this.messageButton.addMouseListener(new MouseInputAdapter() {
+      @Override
+      public void mouseClicked (MouseEvent e) {
+        String message = JOptionPane.showInputDialog("Nachricht");
+
+        if (message != null) {
+          LobbyScreen.this.client.sendChatMessage(message);
+        }
+      }
+    });
   }
 
   @Override
   public void handleMessage (Message message) {
+    if (message instanceof ShowLobby) {
+      this.reloadPlayerList();
+    } else if (message instanceof World.AddPlayerTransform || message instanceof World.RemovePlayerTransform) {
+      this.reloadPlayerList();
+    } else if (message instanceof ChatMessage) {
+      this.chatMessages.add((ChatMessage)message);
+
+      this.reloadChatMessages();
+    }
+  }
+
+  private void reloadChatMessages () {
+    StringBuilder text = new StringBuilder();
+
+    for (ChatMessage message : this.chatMessages) {
+      text.append(String.format("%s: %s\n", message.getAuthor(), message.getText()));
+    }
+
+    this.chatMessageList.setText(text.toString());
+  }
+
+  private void reloadPlayerList () {
 
   }
 }
